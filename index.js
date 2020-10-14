fb08e892a8e6a0ca18e1f76cfe157cf08a3f012a
damenor <damenor.develop@gmail.com>
2020-10-14 05:31:44 +0200

    first version

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  hola: function() { console.log('¡Hola Mundo!') },
+  holaPersonalizado: function(name) { console.log(`¡Hola ${name}!`) },
+  holaCursoNPM: function() { console.log('¡Hola al curso NPM!') },
+}
